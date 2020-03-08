4ac2c3c80b7f3db78f023ac52b99706cfd3cc5cf
Gus <gustavo.canales@me.com>
2020-03-08 12:42:45 +0100

    Initial commitment

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express=require("express");
+const bodyParser=require("body-parser");
+const exphbs  = require('express-handlebars');
+
+const app=express();
+
+const PORT=process.env.PORT || 3000;
+
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+  );
+  app.set("view engine", "handlebars");
+
+  // Routes
+require("./routes/htmlRoutes")(app);
+//require("./routes/apiRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+
+  module.exports=app;
