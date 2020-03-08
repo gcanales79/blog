b9506e388eba69e4b512292fb8c220503b2c32d8
Gus <gustavo.canales@me.com>
2020-03-08 23:51:15 +0100

    Fix layout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,6 +9,22 @@ module.exports = function (app) {
 
     });
 
+    // Load comming soon page
+    app.get("/coming-soon", function (req, res) {
+        res.render("coming", {
+            msg: "Welcome!",
+        });
+
+    });
+
+    // Load Trattoria Da Antonio
+    app.get("/trattoria-da-antonio", function (req, res) {
+        res.render("daAntonio", {
+            msg: "Welcome!",
+        });
+
+    });
+
     // Render 404 page for any unmatched routes
     app.get("*", function (req, res) {
         res.render("404");
